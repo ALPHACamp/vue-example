c384031b19c9edeae792f0ed2f1ad43b5c65df69
Ellen Lee <leechinghui.tw@gmail.com>
2018-07-14 22:17:24 +0800

    vue router exercise

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class>
+    <h1>{{ msg }}</h1>
+    <router-link to="/">home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      msg: "Hello World"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class>
+    <h1>{{ msg }}</h1>
+    <router-link :to="{name:'HelloWorld'}">hello world</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      msg: "Home"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
